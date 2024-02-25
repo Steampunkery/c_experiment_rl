93f292d70a35ddddff9630fbd17ae878556114a2
Thomas O'Keeffe <tlo39@drexel.edu>
2024-02-24 23:44:53 -0500

    map: fix bugs and make items arrays dense

--- src/item.c
+++ src/item.c
@@ -21,46 +21,41 @@ Item mjolnir = {
 ecs_entity_t place_item(ecs_world_t *world, ecs_entity_t e, int x, int y)
 {
     Map *map = ecs_singleton_get_mut(world, Map);
-
-    if (!map || !map_contains(map, x, y)) return 0;
+    if (!map_contains(map, x, y)) return 0;
 
     GArray *items = map->items[y][x];
     if (!items) items = map->items[y][x] = g_array_sized_new(FALSE, TRUE, sizeof(ecs_entity_t), 8);
 
-    guint i;
-    for (i = 0; i < items->len; i++)
-        if (g_array_index(items, ecs_entity_t, i) == 0) break;
-
-    if (i == items->len)
-        g_array_append_val(items, e);
-    else
-        g_array_index(items, ecs_entity_t, i) = e;
-
+    g_array_append_val(items, e);
     ecs_set(world, e, Position, { x, y });
 
     return e;
 }
 
+// Expects the existence of a Map
 ecs_entity_t pickup_item(ecs_world_t *world, ecs_entity_t e, int x, int y)
 {
     Map *map = ecs_singleton_get_mut(world, Map);
-
-    if (!map || !map_contains(map, x, y) || !map->items[y][x]) return 0;
+    if (!map_contains(map, x, y) || !map->items[y][x]) return 0;
 
     GArray *items = map->items[y][x];
+    if (items->len == 0) return 0;
 
     guint i;
-    ecs_entity_t *item;
-    for (i = 0; i < items->len; i++) {
-        item = &g_array_index(items, ecs_entity_t, i);
-        if (*item == e && *item != 0) break;
-        if (e == 0 && *item != 0) break;
+    ecs_entity_t item;
+    if (e == 0) {
+        i = 0;
+        item = g_array_index(items, ecs_entity_t, 0);
+    } else {
+        for (i = 0; i < items->len; i++) {
+            item = g_array_index(items, ecs_entity_t, i);
+            if (item == e) break;
+        }
+        if (i == items->len) return 0;
     }
 
-    if (i == items->len) return 0;
-
-    e = *item;
-    *item = 0;
+    e = item;
+    g_array_remove_index_fast(items, i);
     ecs_remove(world, e, Position);
 
     return e;
@@ -84,7 +79,7 @@ wchar_t get_item_type_glyph(enum item_type item)
 
 ecs_entity_t get_item_type_at_pos(ecs_world_t *world, Map *map, enum item_type type, int x, int y)
 {
-    if (!map || !map_contains(map, x, y)) return 0;
+    if (!map_contains(map, x, y)) return 0;
 
     GArray *items = map->items[y][x];
     if (!items) return 0;
